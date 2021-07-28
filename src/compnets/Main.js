f864d6c60e65b7c704cceb3c4e827c914558e578
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-07-28 20:15:45 +0300

    test

---
+++ src/compnets/Main.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import HornedBeast from './HornedBeast';
+import Data from './data.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+class Main extends React.Component {
+
+  render() {
+    const dataPic = Data.map(picRender => {
+      return (
+        <div>
+          <HornedBeast 
+            title = {picRender.title}
+            description = {picRender.description}
+            image_url = {picRender.image_url}
+            keyword = {picRender.keyword}
+            horns = {picRender.horns}
+            stateUpdate={this.props.stateUpdate}
+            CardData={this.props.CardData}
+          />
+
+        </div>
+
+      );
+    });
+
+    return (
+      <div>
+        {dataPic}
+      </div>);
+  }
+}
+
+export default Main;
