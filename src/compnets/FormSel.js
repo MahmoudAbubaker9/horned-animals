11bfd93bea2c5b3cd6da1cc1be5d792639838bd7
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-07-28 23:42:04 +0300

    edit class04

--- src/compnets/FormSel.js
+++ src/compnets/FormSel.js
@@ -1,20 +1,20 @@
-// import React, { Component } from 'react'
-// import Form from 'react-bootstrap/Form'
+import React, { Component } from 'react'
+import Form from 'react-bootstrap/Form'
 
-// export class FormSel extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Form.Select defaultValue= "0" as="select" custom name='selecteHorn' onChange={this.props.NumberOfH} >
-//   <option>Open this select menu</option>
-//   <option value="1">One</option>
-//   <option value="2">Two</option>
-//   <option value="3">Three</option>
-//   <option value="100">Wow</option>
-//         </Form.Select>
-//             </div>
-//         )
-//     }
-// }
+export class FormSel extends Component {
+    render() {
+        return (
+            <div>
+                <Form.Select defaultValue= "0" as="select" custom name='selecteHorn' onChange={this.props.NumberOfH} >
+  <option>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+  <option value="100">Wow</option>
+        </Form.Select>
+            </div>
+        )
+    }
+}
 
-// export default FormSel
+export default FormSel
